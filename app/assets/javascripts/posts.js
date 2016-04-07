eaeb5a665bee5809194c6bb47c393319f291c82c
Jeff Aschenbrenner <jeffrey.aschenbrenner@gmail.com>
2016-04-06 23:23:57 -0500

    Added support to play/pause gifs on posts#index

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -16,4 +16,14 @@ $(document).ready(function(){
   $('#post_image').on('change', function(){
   	previewImg(this);
   });
+
+  $('.panel.animated .static-img .play').click(function(){
+    $(this).closest('.static-img').addClass('hidden');
+    $(this).closest('.panel-heading').children('.animated-img').removeClass('hidden');
+  });
+
+  $('.panel.animated .animated-img .pause').click(function(){
+    $(this).closest('.animated-img').addClass('hidden');
+    $(this).closest('.panel-heading').children('.static-img').removeClass('hidden');
+  });
 });
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, styles: { large: "1000x1000#", medium: "550x550#" }
+	has_attached_file :image, styles: { large: "1000x1000#", medium: "550x550#", thumb: {geometry: '350x350#', animated: false}, thumb_animated: {geometry: '350x350#', animated: true}}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
