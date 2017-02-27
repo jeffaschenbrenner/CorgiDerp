405889b1089d00b9b9bfe62f333ab38eaa6fc2d4
Jeff Aschenbrenner <jeffrey.aschenbrenner@gmail.com>
2017-02-27 13:26:36 -0600

    Remove aws config from version control

--- config/initializers/aws_s3.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.production?
-  ENV['AWS_BUCKET'] = 'corgiderp'
-  ENV['AWS_ACCESS_KEY_ID'] = 'AKIAI2DXTZCRSU2IGNAA'
-  ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-end
